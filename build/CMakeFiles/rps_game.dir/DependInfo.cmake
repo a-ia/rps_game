c9f897c342a0a95144e7964a6493e1d22d5fd5e3
a-ia <sharonsudan@outlook.com>
2025-02-11 16:05:19 -0500

    Added some comments

---
+++ build/CMakeFiles/rps_game.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/angel/Documents/game eng/Assignment 2 Sharon Sudan/rps_game/game.cpp" "CMakeFiles/rps_game.dir/game.cpp.o" "gcc" "CMakeFiles/rps_game.dir/game.cpp.o.d"
+  "/home/angel/Documents/game eng/Assignment 2 Sharon Sudan/rps_game/main.cpp" "CMakeFiles/rps_game.dir/main.cpp.o" "gcc" "CMakeFiles/rps_game.dir/main.cpp.o.d"
+  "" "rps_game" "gcc" "CMakeFiles/rps_game.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/rps_game.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rps_game.dir/link.d"
+  "CMakeFiles/rps_game.dir/game.cpp.o"
+  "CMakeFiles/rps_game.dir/game.cpp.o.d"
+  "CMakeFiles/rps_game.dir/main.cpp.o"
+  "CMakeFiles/rps_game.dir/main.cpp.o.d"
+  "rps_game"
+  "rps_game.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rps_game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
